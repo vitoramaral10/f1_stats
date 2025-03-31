817504f07a5ba16aaf3af22016e712cb923e50fe
Vitor Melo <vitor.melo@cmsw.com>
2025-03-31 09:12:10 -0300

    chore: remove unused files and configurations across platforms

---
+++ android/app/src/main/kotlin/br/dev/vitormelo/f1_stats/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.dev.vitormelo.f1_stats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/f1_stats/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.f1_stats
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
